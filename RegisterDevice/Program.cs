848630e32a65ba6febc31ade5cf13766c957e177
digitaldias <pedro@digitaldias.com>
2016-11-11 12:55:20 +0100

    Alt sammen i  en bolk

---
+++ RegisterDevice/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System;
+
+namespace RegisterDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Forferdelig!");
+                return;
+            }
+
+
+            // Utfør registrering i Azure
+            string connectionString = "HostName=demo-iothub-pedro.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=MLwdCRkEtgcdwV+Cyj3zV//OUG+4zVdsVrwvWRnhnjk=";
+            var registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+
+            Device device = new Device(args[0]);
+            Device registered = null;
+
+            try
+            {
+                registered = registryManager.AddDeviceAsync(device).Result;
+            }
+            catch(AggregateException ex)
+            {
+                if(ex.InnerException.GetType() == typeof(DeviceAlreadyExistsException))
+                {
+                    Console.WriteLine($"Device {args[0]} allerede registrert");
+                    registered = registryManager.GetDeviceAsync(args[0]).Result;
+                }
+            }
+
+            Console.WriteLine($"Enhet registert: {registered.Authentication.SymmetricKey.PrimaryKey}");
+            Console.WriteLine("Ferdig, trykk ANY tasten for å fortsette");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Simulator/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Simulator.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double _sliderValue;
+        private double _transmittedValue;
+        private DispatcherTimer _updateTimer;
+
+        public MainPageViewModel()
+        {
+            _updateTimer          = new DispatcherTimer();
+            _updateTimer.Interval = TimeSpan.FromSeconds(11);
+            _updateTimer.Tick += SendValuesToIoTHub;
+            _updateTimer.Start();
+        }
+
+
+        private async void SendValuesToIoTHub(object sender, object e)
+        {
+            var melding = new
+            {
+                deviceId = "WorkshopDevice", 
+                verdi = SliderValue
+            };
+            var kodetMelding = JsonConvert.SerializeObject(melding);
+
+            var message = new Message(Encoding.ASCII.GetBytes(kodetMelding));
+
+
+            string connectionstring = "HostName=demo-iothub-pedro.azure-devices.net;DeviceId=WorkshopDevice;SharedAccessKey=LIB5qteYzkg5D86kG8mecesNXbCmW84exJtB2O8DdHQ=";
+            var deviceClient = DeviceClient.CreateFromConnectionString(connectionstring);
+
+            
+
+            await deviceClient.SendEventAsync(message);
+
+            TransmittedValue = SliderValue;
+        }
+
+
+        public void ShoutAbout([CallerMemberName] string propertyName  ="Unknown")
+        {
+            if (string.IsNullOrEmpty(propertyName))
+                return;
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public double SliderValue
+        {
+            get { return _sliderValue; }
+            set { _sliderValue = value; ShoutAbout(); }
+        }
+
+        public double TransmittedValue
+        {
+            get { return _transmittedValue; }
+            set { _transmittedValue = value; ShoutAbout(); }
+        }
+    }
+}
